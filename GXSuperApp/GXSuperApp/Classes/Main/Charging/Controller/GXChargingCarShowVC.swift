a985392f670372e34c572826d4efa48ac7d16165
Gin <279694479@qq.com>
2024-07-10 14:21:30 +0800

    充电的动画完成

--- GXSuperApp/GXSuperApp/Classes/Main/Charging/Controller/GXChargingCarShowVC.swift
+++ GXSuperApp/GXSuperApp/Classes/Main/Charging/Controller/GXChargingCarShowVC.swift
@@ -18,6 +18,17 @@ class GXChargingCarShowVC: GXBaseViewController {
     
     private var displayLink: CADisplayLink?
     private var progressConstant: CGFloat = 0
+    private var maxProgressConstant: CGFloat {
+        return SCREEN_WIDTH - 110.0
+    }
+    private var currentProgress: Int {
+        let progress = 1.0 - self.progressBarRightLC.constant / self.maxProgressConstant
+        return Int(ceil(progress * 100.0))
+    }
+    
+    deinit {
+        self.stopAnimation()
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -37,54 +48,45 @@ class GXChargingCarShowVC: GXBaseViewController {
         let colors: [UIColor] = [.gx_green, .init(hexString: "#278CFF")]
         let imageSize = CGSize(width: 10, height: 40.0)
         self.progressBarIView.image = UIImage(gradientColors: colors, style: .horizontal, size: imageSize)
-        self.progressBarRightLC.constant = self.progressBarView.width - 80
         self.progressLabel.text = "0%"
+        self.view.layoutSubviews()
+        self.progressBarRightLC.constant = self.maxProgressConstant
     }
-    
-    
-    
+        
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // 触发更新约束的函数
-        updateBarProgress(to: 1.0) // 将视图的宽度更新为200点，并应用动画
+        if abs(self.progressBarRightLC.constant - self.maxProgressConstant) <= 2 {
+            self.updateBarProgress(to: 1.0)
+        }
+        else {
+            self.updateBarProgress(to: 0.0)
+        }
     }
     
 }
 
 extension GXChargingCarShowVC {
-    
     func updateBarProgress(to progress: CGFloat) {
-        let maxWith: CGFloat = progressBarView.width - 80.0
+        let maxWith = self.maxProgressConstant
         let constant = maxWith - maxWith * progress
         self.progressConstant = constant
-//        self.progressBarRightLC.constant = constant
-//        UIView.animate(withDuration: 5) {
-//            self.view.layoutIfNeeded()
-//        }
         self.startAnimation()
     }
-    
+    func stopAnimation() {
+        self.displayLink?.invalidate()
+        self.displayLink = nil
+    }
     func startAnimation() {
         self.displayLink = CADisplayLink(target: self, selector: #selector(handleDisplayLink(displayLink:)))
         self.displayLink?.add(to: .current, forMode: .common)
         self.displayLink?.preferredFramesPerSecond = 60
     }
-    
     @objc func handleDisplayLink(displayLink: CADisplayLink) {
-        let maxWith: CGFloat = progressBarView.width - 80.0
-        let progress = 1.0 - self.progressBarRightLC.constant / maxWith
-        let progressInt = Int(ceil(progress * 100.0))
-        self.progressLabel.text = "\(progressInt)%"
-
-        let speed: CGFloat = self.progressBarRightLC.constant > self.progressConstant ? -2 : 2
+        self.progressLabel.text = "\(self.currentProgress)%"
+        let speed: CGFloat = self.progressBarRightLC.constant >= self.progressConstant ? -2 : 2
         if abs(self.progressBarRightLC.constant - self.progressConstant) > abs(speed) {
             self.progressBarRightLC.constant += speed
         } else {
             self.stopAnimation()
         }
-    }
-    
-    func stopAnimation() {
-        self.displayLink?.invalidate()
-        self.displayLink = nil
     }
 }
