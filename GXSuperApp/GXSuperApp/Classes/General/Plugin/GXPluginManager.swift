8594215c70b5a65812cc0a97be2222c2e3a118ec
Gin <279694479@qq.com>
2024-09-02 23:49:49 +0800

    1

--- GXSuperApp/GXSuperApp/Classes/General/Plugin/GXPluginManager.swift
+++ GXSuperApp/GXSuperApp/Classes/General/Plugin/GXPluginManager.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 open class GXPluginManager: NSObject {
+    private let pluginLock = NSLock()
     
     private var plugins: [String: GXPluginProtocol] = [:]
 
@@ -17,20 +18,49 @@ open class GXPluginManager: NSObject {
     
     public override init() {}
     
-    public class func register(plugin: GXPluginProtocol, forKey key: String) {
-        GXPluginManager.shared.plugins[key] = plugin
+    public func register(plugin: GXPluginProtocol, forKey key: String) {
+        self.pluginLock.lock()
+        self.plugins[key] = plugin
+        self.pluginLock.unlock()
     }
     
-    public class func plugin(key: String) -> GXPluginProtocol? {
-        return GXPluginManager.shared.plugins[key]
+    public func plugin(key: String) -> GXPluginProtocol? {
+        self.pluginLock.lock()
+        let plugin = self.plugins[key]
+        self.pluginLock.lock()
+
+        return plugin
     }
     
-    public class func register(pluginAction: @escaping GXPluginAction, forKey key: String) {
-        GXPluginManager.shared.pluginActions[key] = pluginAction
+    public func register(pluginAction: @escaping GXPluginAction, forKey key: String) {
+        self.pluginLock.lock()
+        self.pluginActions[key] = pluginAction
+        self.pluginLock.unlock()
     }
     
-    public class func pluginAction(key: String) -> GXPluginAction? {
-        return GXPluginManager.shared.pluginActions[key]
+    public func pluginAction(key: String) -> GXPluginAction? {
+        self.pluginLock.lock()
+        let pluginAction = self.pluginActions[key]
+        self.pluginLock.lock()
+
+        return pluginAction
+    }
+}
+ 
+public extension GXPluginManager {
+    class func register(plugin: GXPluginProtocol, forKey key: String) {
+        GXPluginManager.shared.register(plugin: plugin, forKey: key)
     }
     
+    class func plugin(key: String) -> GXPluginProtocol? {
+        return GXPluginManager.shared.plugin(key: key)
+    }
+    
+    class func register(pluginAction: @escaping GXPluginAction, forKey key: String) {
+        GXPluginManager.shared.register(pluginAction: pluginAction, forKey: key)
+    }
+    
+    class func pluginAction(key: String) -> GXPluginAction? {
+        return GXPluginManager.shared.pluginAction(key: key)
+    }
 }
